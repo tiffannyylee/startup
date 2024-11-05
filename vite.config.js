89d2dc5595fd7f42b16789e65274708d6fa6418e
tiffannyylee <tifflee@byu.edu>
2024-11-05 11:11:10 -0700

    added the ability for vite, made vite config

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    open: false // Change to true if you want it to open automatically
+  }
+});
