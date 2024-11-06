d2c065d1c4b938cf053d32d61f406628cf53033e
tiffannyylee <tifflee@byu.edu>
2024-11-06 11:51:26 -0700

    added login budget and payments jsx

---
+++ src/payments/payments.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Payments() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>payments displayed here</div>
+    </main>
+  );
+}
