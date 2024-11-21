ad8fa5934f83f30c5c6ac134f280c4af8a8b96a6
tiffannyylee <tifflee@byu.edu>
2024-11-20 20:50:26 -0700

    api

--- service/index.js
+++ service/index.js
@@ -7,10 +7,13 @@ let users = {};
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
 app.use(express.json());
-
 // Router for service endpoints
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
+
+app.get('/', (_req, res) => {
+  res.send({ msg: 'Server is working' });
+});
 
 // CreateAuth a new user
 apiRouter.post('/auth/create', async (req, res) => {
@@ -18,35 +21,12 @@ apiRouter.post('/auth/create', async (req, res) => {
   if (user) {
     res.status(409).send({ msg: 'Existing user' });
   } else {
-    const user = { email: req.body.email, password: req.body.password, token: uuid.v4() };
-    users[user.email] = user;
+    const newUser = { email: req.body.email, password: req.body.password, token: uuid.v4() };
+    users[newUser.email] = newUser;
 
-    res.send({ token: user.token });
+    res.send({ token: newUser.token });
   }
 });
-
-// GetAuth login an existing user
-apiRouter.post('/auth/login', async (req, res) => {
-  const user = users[req.body.email];
-  if (user) {
-    if (req.body.password === user.password) {
-      user.token = uuid.v4();
-      res.send({ token: user.token });
-      return;
-    }
-  }
-  res.status(401).send({ msg: 'Unauthorized' });
-});
-
-// DeleteAuth logout a user
-apiRouter.delete('/auth/logout', (req, res) => {
-  const user = Object.values(users).find((u) => u.token === req.body.token);
-  if (user) {
-    delete user.token;
-  }
-  res.status(204).end();
-});
-
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
