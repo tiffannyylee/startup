af8f486b5afdfbb5ffc78ac23139f80e4e037afc
tiffannyylee <tifflee@byu.edu>
2024-11-23 10:07:19 -0700

    changed config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
-// vite.config.js
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
     },
-  }
+  },
+  plugins: [react()],
+  optimizeDeps: {
+    include: ['react-bootstrap'], // Ensures react-bootstrap is bundled correctly
+  },
+  build: {
+    commonjsOptions: {
+      ignoreDynamicRequires: true, // Prevent issues with dynamic imports
+    },
+  },
 });
