c959098e03171077370d68ac36a23e5e1f9fd863
tiffannyylee <tifflee@byu.edu>
2024-12-07 15:52:23 -0700

    change vite back

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,29 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       '/api': 'http://localhost:3000',
+//     },
+//   },
+//   plugins: [react()],
+//   optimizeDeps: {
+//     include: ['react-bootstrap'], // Ensures react-bootstrap is bundled correctly
+//   },
+//   build: {
+//     commonjsOptions: {
+//       ignoreDynamicRequires: true, // Prevent issues with dynamic imports
+//     },
+//   },
+// });
+import { defineConfig } from 'vite';
+
 export default defineConfig({
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
-    },
-  },
-  plugins: [react()],
-  optimizeDeps: {
-    include: ['react-bootstrap'], // Ensures react-bootstrap is bundled correctly
-  },
-  build: {
-    commonjsOptions: {
-      ignoreDynamicRequires: true, // Prevent issues with dynamic imports
     },
   },
 });
