8ec442a2159a97ab1778a98d1ddeafa6d2919681
tiffannyylee <tifflee@byu.edu>
2024-12-07 16:26:43 -0700

    new mongo

--- service/serviceDatabase.js
+++ service/serviceDatabase.js
@@ -2,9 +2,9 @@ const { MongoClient } = require('mongodb');
 const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 const config = require('./dbConfig.json');
-
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
-const client = new MongoClient(url);
+//const client = new MongoClient(url);
+const client = new MongoClient(url, { tls: true, serverSelectionTimeoutMS: 3000, autoSelectFamily: false, });
 // const db = client.db('startup');
 // const userCollection = db.collection('user');
 // const budgetCollection = db.collection('budget');
