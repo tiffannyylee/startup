95e66b52fc77d170b9051b34750ce82d8d1a0dbf
tiffannyylee <tifflee@byu.edu>
2024-12-09 19:28:00 -0700

    comment out mock websocket

--- src/payments/mockWebsocket.jsx
+++ src/payments/mockWebsocket.jsx
@@ -1,21 +1,21 @@
-// mockWebSocket.js
-import { useEffect } from 'react';
+// // mockWebSocket.js
+// import { useEffect } from 'react';
 
-export function MockWebSocket({ onMessage }) {
-  useEffect(() => {
-    // Mock: Send a "new payment" every 5 seconds
-    const interval = setInterval(() => {
-      const fakePayment = {
-        id: Math.random().toString(36).substr(2, 9),
-        amount: Math.floor(Math.random() * 100) + 1,
-        bucket: 'bucket' + (Math.floor(Math.random() * 3) + 1),
-        timestamp: new Date().toLocaleTimeString(),
-      };
-      onMessage(fakePayment);
-    }, 2000);
+// export function MockWebSocket({ onMessage }) {
+//   useEffect(() => {
+//     // Mock: Send a "new payment" every 5 seconds
+//     const interval = setInterval(() => {
+//       const fakePayment = {
+//         id: Math.random().toString(36).substr(2, 9),
+//         amount: Math.floor(Math.random() * 100) + 1,
+//         bucket: 'bucket' + (Math.floor(Math.random() * 3) + 1),
+//         timestamp: new Date().toLocaleTimeString(),
+//       };
+//       onMessage(fakePayment);
+//     }, 2000);
 
-    return () => clearInterval(interval);
-  }, [onMessage]);
+//     return () => clearInterval(interval);
+//   }, [onMessage]);
 
-  return null;
-}
+//   return null;
+// }
